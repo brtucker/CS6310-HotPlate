4781dc2722640066e1e3cfb8348a9b6baae08c08
mgodfreyyoung <myoung68@gatech.edu>
2014-09-16 17:04:47 -0600

    just checking in before work incase

--- CS6290-HotPlate/src/com/gatech/cs6310/project1/gui/GUI.java
+++ CS6290-HotPlate/src/com/gatech/cs6310/project1/gui/GUI.java
@@ -1,58 +1,88 @@
 package com.gatech.cs6310.project1.gui;
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
-import com.gatech.cs6310.project1.experiments.diffusion;
 
-public class GUI extends JPanel {
+public class GUI extends JPanel{
 	
 	
-	diffusion diffusionMethod;
+	public void paintComponent(Graphics g) {
+		  // super.paintComponent(g);
+		   Graphics2D g2d = (Graphics2D)g;
+		   g.drawOval(10, 10, 10, 10);
+		  
+		}
+	
+	public static void addComponentsToPane(Container pane) {
+		pane.setLayout(null);
 
-	public GUI() {
-		String[] equipermentList = { "Tpdahp", "Tpfahp", "Twfahp","Tpdohp" };
+		String[] equipermentList = { "Tpdahp", "Tpfahp", "Twfahp", "Tpdohp" };
 		JComboBox simulationList = new JComboBox(equipermentList);
 		JButton runSimulationButton = new JButton("Run");
+		final JTextArea editTextDimension = new JTextArea("D");		
 		JTextArea editTextLeft = new JTextArea("L");
 		JTextArea editTextRight = new JTextArea("R");
 		JTextArea editTextTop = new JTextArea("T");
 		JTextArea editTextBottom = new JTextArea("B");
-		JTextArea outputWindow = new JTextArea("Sample Output");
+		final JTextArea outputWindow = new JTextArea("Sample Output");
+
+		pane.add(simulationList);
+		pane.add(runSimulationButton);
+		pane.add(editTextDimension);
+		pane.add(editTextBottom);
+		pane.add(editTextTop);
+		pane.add(editTextRight);
+		pane.add(editTextLeft);
+		pane.add(outputWindow);
+
+		Insets insets = pane.getInsets();
+		Dimension size = simulationList.getPreferredSize();
+
+		simulationList.setBounds(25 + insets.left, 5 + insets.top, size.width,
+				size.height);
+		size = runSimulationButton.getPreferredSize();
+		runSimulationButton.setBounds(55 + insets.left, 40 + insets.top, size.width, size.height);
+		size = outputWindow.getPreferredSize();
+		outputWindow.setBounds(20 + insets.left, 250 + insets.top, size.width + 500,
+				size.height + 100);
+		size = editTextDimension.getPreferredSize();
+		editTextDimension.setBounds(200 + insets.left, 125 + insets.top, size.width + 50,
+				size.height);
+		editTextLeft.setBounds(300 + insets.left, 125 + insets.top, size.width + 50,
+				size.height);
+		editTextRight.setBounds(440 + insets.left, 125 + insets.top, size.width + 50,
+				size.height);
 		
+		editTextTop.setBounds(370 + insets.left, 75 + insets.top, size.width + 50,
+				size.height);
+		
+		editTextBottom.setBounds(370 + insets.left, 175 + insets.top, size.width + 50,
+				size.height);
+		
+		runSimulationButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+				outputWindow.setText(editTextDimension.getText());
+			}
+		});
 		
 		simulationList.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				JComboBox tmpCombo = (JComboBox) e.getSource();
-				String returnItem = (String) tmpCombo.getSelectedItem();
+				outputWindow.setText((String) tmpCombo.getSelectedItem());
 			}
 		});
-		this.setLayout(new BorderLayout());
-		add(simulationList, BorderLayout.LINE_START);
+
 		
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.setSize(100, 100);
-		panel.add(editTextLeft, BorderLayout.EAST);
-		panel.add(editTextTop, BorderLayout.NORTH);
-		panel.add(editTextRight, BorderLayout.SOUTH);
-		panel.add(editTextBottom, BorderLayout.WEST);
-		add(panel, BorderLayout.CENTER);	
-		JPanel bpanel = new JPanel(new BorderLayout());		
-		bpanel.add(runSimulationButton, BorderLayout.PAGE_START);
-		bpanel.add(outputWindow, BorderLayout.PAGE_END);
-		add(bpanel,BorderLayout.PAGE_END);
-
-
-//		
-//		JTextArea e = new JTextArea("Basdfdsf");
-//		panel.add(e,BorderLayout.PAGE_END);
-
-
 		
 
 	}
-	
-	
+
+
+
+
 
 }
