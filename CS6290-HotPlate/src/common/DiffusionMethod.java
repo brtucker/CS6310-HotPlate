2a893354a639fde7c263bd0c75fd0fd0e5aefd1a
Dan Holmes <dnhlms@gmail.com>
2014-09-20 09:32:14 -0400

    changed maxIteration propertry to be 800 by default.
    It can be overridden by the values on the UI.

--- CS6290-HotPlate/src/common/DiffusionMethod.java
+++ CS6290-HotPlate/src/common/DiffusionMethod.java
@@ -3,7 +3,7 @@ package common;
 
 public abstract class DiffusionMethod {
 	public int maxDuration = 0;
-	public int maxIterations = 0;
+	public int maxIterations = 800;
 	public double stabilizationDelta = 1.0;
 	
 	abstract public SimulationResult simulate(int dimension, double tempLeft,
